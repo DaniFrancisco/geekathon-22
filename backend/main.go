797479b4868f8a8bad8810d8f2d0cadf50923eac
João Almeida <jmdalmeida92@gmail.com>
2022-11-06 12:31:49 +0000

    sound handler

--- backend/main.go
+++ backend/main.go
@@ -2,27 +2,33 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	// Echo instance
 	e := echo.New()
 
-	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Routes
 	e.GET("/", hello)
+	e.GET("/sound", sound)
 
-	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-// Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func sound(c echo.Context) error {
+	f, err := os.Open("audio_tour.wav")
+	if err != nil {
+		return err
+	}
+
+	return c.Stream(http.StatusOK, "audio/wav", f)
 }
